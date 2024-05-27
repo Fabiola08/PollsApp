9e3c07062d3d75fe1871a65ba57a547607365ffa
Fabiola08 <alejandrafabiolalopez58@gmail.com>
2024-05-27 11:23:54 -0600

    Implementación de validaciones

--- pollsApp/polls/views.py
+++ pollsApp/polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 
 
 from .models import Question, Choice
+from django.http import Http404
 # Create your views here.
 
 def home(request):
@@ -11,21 +12,28 @@ def home(request):
     })
 
 def vote(request, q_id):
-    question = Question.objects.get(id=q_id)
-
+    question = get_object_or_404(Question, id=q_id)
     if request.method == 'POST':
-        choice = request.POST['choice']
-        
-        c = Choice.objects.get(id=choice)
-        c.votes += 1
-        c.save()
+        try:
+            choice = request.POST['choice']
+            c = Choice.objects.get(id=choice)
+            c.votes += 1
+            c.save()
+            return redirect('polls:results', q_id)
+        except(KeyError):
+            return render(request, 'polls/question.html', { "question" : question, "error_message": "Debes seleccionar una opción!"})
 
-        return redirect('polls:results', q_id)
-
-    return render(request, 'polls/question.html',
-                  { "question" : question})
+    return render(request, 'polls/question.html', { "question": question})
 
 def results(request, q_id):
     question = Question.objects.get(id=q_id)
+    return render(request, 'polls/results.html',
+                  { "question": question})
+
+def results(request, q_id):
+    try:
+        question = Question.objects.get(id=q_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return render(request, 'polls/results.html',
                   { "question": question})
